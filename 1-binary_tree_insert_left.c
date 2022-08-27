54af6320232c7e916b9e5db1a47cc21728d4df6e
odilon-cloud <oirambna@gmail.com>
2022-08-27 21:14:10 +0000

    commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,17 +10,20 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node = NULL;
+	binary_tree_t *new_left_node;
 
 	if (parent == NULL)
 		return (NULL);
 
 	new_left_node = binary_tree_node(parent, value);
+	if (new_left_node == NULL)
+		return (NULL);
 
-	new_left_node->left = parent->left;
-	if (new_left_node->left != NULL)
-		new_left_node->left->parent = new_left_node;
-
+	if (parent->left != NULL)
+	{
+		new_left_node->left = parent->left;
+		parent->left->parent = new_left_node;
+	}
 	parent->left = new_left_node;
 	return (new_left_node);
 }
