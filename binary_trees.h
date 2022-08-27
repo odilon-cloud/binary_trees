9c3483e594f0cd91bdf480f2008e0296c2191b70
odilon-cloud <oirambna@gmail.com>
2022-08-27 12:35:38 +0000

    updating the h file

--- binary_trees.h
+++ binary_trees.h
@@ -40,6 +40,52 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* deletes entire binary tree */
 void binary_tree_delete(binary_tree_t *tree);
 
+/* checks if a node is a leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* checks if a node is a root */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* goes through binary tree using pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* goes through binary tree using in-order traversal */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* goes through binary tree using post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* measures the height of a binary tree */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* measures the depth of a node in a binary tree */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* measures the size of a binary tree */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/* counts the leaves in a binary tree */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/* counts the nodes with at least one child */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/* measures the balance factor */
+int binary_tree_balance(const binary_tree_t *tree);
+
+/* checks if binary tree is full */
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/* checks if binary tree is perfect */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* finds the sibling of a node */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* finds the uncle of a node */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
+
+#endif
 #endif
