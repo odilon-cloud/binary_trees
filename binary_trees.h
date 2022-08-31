2908c493483d805d3239745dd5c1145fe55916a8
odilon-cloud <oirambna@gmail.com>
2022-08-31 12:50:32 +0000

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -85,6 +85,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /* finds the uncle of a node */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/*finds the lowest common ancestor of two nodes*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+const binary_tree_t *second);
 
 
 #endif
